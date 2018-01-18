71c615e504f2c442fa17645c5800adc517e0d11e
richard.liu <richard.liu@cloudminds.com>
2018-01-18 16:17:07 +0800

    fix bug of connect with docker mysql

--- UserService/src/main/resources/db/data.sql
+++ UserService/src/main/resources/db/data.sql
@@ -11,12 +11,3 @@ INSERT INTO user_roles(user_id, roles_id) VALUES(1, 1);
 
 INSERT INTO role_authorities(role_id, authorities_id) VALUES(1, 1);
 INSERT INTO role_authorities(role_id, authorities_id) VALUES(1, 2);
-
-create table t_user(
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
-  user_name VARCHAR(30) not null,
-  credits INT,
-  password VARCHAR(32) not null,
-  last_visit DATETIME,
-  last_ip VARCHAR(23)
-)
